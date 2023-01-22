2a8310688bfcce9c0b46b3358b69985954517fcf
IKoshelev <koshelew@live.com>
2023-01-22 19:41:54 +0100

    color filter works

--- main.ts
+++ main.ts
@@ -13,13 +13,35 @@ const DEFAULT_SETTINGS: MyPluginSettings = {
 
 export function nodeBondingBoxContains(outerNode: CanvasNodeData, innerNode: CanvasNodeData) {
 	return outerNode.x <= innerNode.x
-			&& (outerNode.x + outerNode.width) >= (innerNode.x + innerNode.width)
-			&& outerNode.y <= innerNode.y
-			&& (outerNode.y + outerNode.height) >= (innerNode.y + innerNode.height);
+		&& (outerNode.x + outerNode.width) >= (innerNode.x + innerNode.width)
+		&& outerNode.y <= innerNode.y
+		&& (outerNode.y + outerNode.height) >= (innerNode.y + innerNode.height);
 }
 
-export function showOnlyNodes(canvas: any, idsToShow: Set<string>) {
+export function showOnlyNodes(canvas: any, idsToShow?: Set<string>) {
+	const nodes = canvas.nodes.values();
 
+	for (const node of nodes) {
+		if (idsToShow === undefined || idsToShow.has(node.id)) {
+			node.nodeEl.show();
+		} else {
+			node.nodeEl.hide();
+		}
+	}
+}
+
+export function showOnlyEdges(canvas: any, idsToShow?: Set<string>) {
+	const edges = canvas.edges.values();
+
+	for (const edge of edges) {
+		if (idsToShow === undefined || idsToShow.has(edge.id)) {
+			edge.lineGroupEl.style.display = "";
+			edge.markerGroupEl.style.display = "";
+		} else {
+			edge.lineGroupEl.style.display = "none";
+			edge.markerGroupEl.style.display = "none";
+		}
+	}
 }
 
 export default class MyPlugin extends Plugin {
@@ -29,8 +51,43 @@ export default class MyPlugin extends Plugin {
 	async onload() {
 
 		this.addCommand({
+			id: 'show-all',
+			name: 'Show all nodes and groups',
+			checkCallback: (checking: boolean) => {
+				const canvasView = this.app.workspace.getActiveViewOfType(ItemView);
+
+				if (canvasView?.getViewType() !== 'canvas') {
+					if (checking) {
+						return false;
+					}
+					return;
+				}
+
+				if (checking) {
+					return true;
+				}
+
+				const canvas = (canvasView as any).canvas;
+				if (!canvas) {
+					return;
+				};
+
+				const canvasData = canvas.getData() as CanvasData;
+
+				if (!canvasData) {
+					return;
+				};
+
+				showOnlyNodes(canvas);
+
+				showOnlyEdges(canvas);
+			}
+		});
+
+
+		this.addCommand({
 			id: 'show-only-same-color',
-			name: 'Show only same color items',
+			name: 'Show only same color nodes and their groups',
 			checkCallback: (checking: boolean) => {
 				const canvasView = this.app.workspace.getActiveViewOfType(ItemView);
 
@@ -72,34 +129,23 @@ export default class MyPlugin extends Plugin {
 
 				const nodes = canvasData.nodes;
 
-				for (const node of nodes) {
-					canvas.nodes.get(node.id).nodeEl.hide()
-				}
+				const fileNodesToShow =
+					nodes.filter(x => x.type === 'file'
+						&& colorsToShow.has(x.color ?? ""));
 
-				const fileNodesToShow = 
-					nodes.filter(x => x.type === 'file' 
-									&& colorsToShow.has(x.color ?? ""));
-
-				const groupNodesToShow = 
-					nodes.filter(x => x.type === 'group' 
-					&& fileNodesToShow.some(fn => nodeBondingBoxContains(x, fn)));
-
-				for (const node of [...fileNodesToShow, ...groupNodesToShow]) {
-					const nodeOnCanvas = canvas.nodes.get(node.id);					
-					nodeOnCanvas.nodeEl.show();
-				}
+				const groupNodesToShow =
+					nodes.filter(x => x.type === 'group'
+						&& fileNodesToShow.some(fn => nodeBondingBoxContains(x, fn)));
 
 				const shownNodeIds = new Set([...fileNodesToShow, ...groupNodesToShow].map(x => x.id));
-				const edges = canvasData.edges;
+				showOnlyNodes(canvas, shownNodeIds);
 
-				for (const edge of edges) {
-					const edgeOnCanvas = canvas.edges.get(edge.id);
-					if (shownNodeIds.has(edge.fromNode) && shownNodeIds.has(edge.toNode)) {
-						delete edgeOnCanvas.lineGroupEl.style.display;
-					} else {
-						edgeOnCanvas.lineGroupEl.style.display = "none";
-					}
-				}
+				const shownEdgeIds = new Set(canvasData.edges
+					.filter(edge => shownNodeIds.has(edge.fromNode)
+						&& shownNodeIds.has(edge.toNode))
+					.map(x => x.id))
+
+				showOnlyEdges(canvas, shownEdgeIds);
 			}
 		});
 	}
