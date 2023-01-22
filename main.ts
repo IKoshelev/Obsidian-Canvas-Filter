da458c2cbf3ef9c7e25ba5294d5c96b2086922a3
IKoshelev <koshelew@live.com>
2023-01-22 20:28:48 +0100

    Added arrows filter

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { CanvasData, CanvasNodeData } from 'canvas';
+import { AllCanvasNodeData, CanvasData, CanvasNodeData } from 'canvas';
 import { App, Editor, ItemView, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
 
@@ -43,6 +43,11 @@ export function showOnlyEdges(canvas: any, idsToShow?: Set<string>) {
 	}
 }
 
+function getGroupsFor(allNodes: AllCanvasNodeData[], nonGroupNodes: AllCanvasNodeData[]) {
+	return allNodes.filter(x => x.type === 'group'
+		&& nonGroupNodes.some(fn => nodeBondingBoxContains(x, fn)));
+}
+
 export default class CanvasFilterPlugin extends Plugin {
 
 	settings: CanvasFilterPluginSettings;
@@ -75,11 +80,65 @@ export default class CanvasFilterPlugin extends Plugin {
 		return commandFn(canvas, canvasData);
 	}
 
+	private showConnectedNodes = (
+		canvas: any, 
+		canvasData: CanvasData, 
+		showUpstreamNodes: boolean, 
+		showDownstreamNodes: boolean) => {
+			const selection: any = Array.from(canvas.selection);
+			if (selection.length === 0) {
+				new Notice("Please select at least one node");
+				return;
+			}
+
+			const nodesIdsToShow = new Set(selection.map((x: any) => x.id).filter((x: any) => x) as string[]);
+			const edgesIdsToShow = new Set<string>();
+			const addedNodes = new Set(nodesIdsToShow);
+			while (addedNodes.size > 0) {
+				const previousAddedNodes = new Set(addedNodes);
+				addedNodes.clear();
+
+				if (showUpstreamNodes) {
+					const outgoingEdges = canvasData.edges.filter(x => previousAddedNodes.has(x.fromNode));
+					for (const edge of outgoingEdges) {
+						edgesIdsToShow.add(edge.id);
+						if (!nodesIdsToShow.has(edge.toNode)) {
+							nodesIdsToShow.add(edge.toNode);
+							addedNodes.add(edge.toNode);
+						}
+					}
+				}
+
+				if (showDownstreamNodes) {
+					const incomingEdges = canvasData.edges.filter(x => previousAddedNodes.has(x.toNode));
+					for (const edge of incomingEdges) {
+						edgesIdsToShow.add(edge.id);
+						if (!nodesIdsToShow.has(edge.fromNode)) {
+							nodesIdsToShow.add(edge.fromNode);
+							addedNodes.add(edge.fromNode);
+						}
+					}
+				}
+			}
+
+			const groupNodesToShow = getGroupsFor(
+				canvasData.nodes, 
+				canvasData.nodes.filter(x => nodesIdsToShow.has(x.id)));
+
+			for (const node of groupNodesToShow) {
+				nodesIdsToShow.add(node.id);
+			}
+
+			showOnlyNodes(canvas, nodesIdsToShow);
+
+			showOnlyEdges(canvas, edgesIdsToShow);
+	}
+
 	async onload() {
 
 		this.addCommand({
 			id: 'show-all',
-			name: 'Show all nodes and groups',
+			name: 'Show nodes and groups: ALL',
 			checkCallback: this.ifActiveViewIsCanvas((canvas, canvasData) => {
 
 				showOnlyNodes(canvas);
@@ -90,7 +149,7 @@ export default class CanvasFilterPlugin extends Plugin {
 
 		this.addCommand({
 			id: 'show-only-same-color',
-			name: 'Show only same color nodes and their groups',
+			name: 'Show nodes and groups: match selected COLOR',
 			checkCallback: this.ifActiveViewIsCanvas((canvas, canvasData) => {
 
 				const selection: any = Array.from(canvas.selection);
@@ -107,15 +166,13 @@ export default class CanvasFilterPlugin extends Plugin {
 
 				const nodes = canvasData.nodes;
 
-				const fileNodesToShow =
-					nodes.filter(x => x.type === 'file'
+				const nonGroupNodesToShow =
+					nodes.filter(x => x.type !== 'group'
 						&& colorsToShow.has(x.color ?? ""));
 
-				const groupNodesToShow =
-					nodes.filter(x => x.type === 'group'
-						&& fileNodesToShow.some(fn => nodeBondingBoxContains(x, fn)));
+				const groupNodesToShow = getGroupsFor(nodes, nonGroupNodesToShow);
 
-				const shownNodeIds = new Set([...fileNodesToShow, ...groupNodesToShow].map(x => x.id));
+				const shownNodeIds = new Set([...nonGroupNodesToShow, ...groupNodesToShow].map(x => x.id));
 				showOnlyNodes(canvas, shownNodeIds);
 
 				const shownEdgeIds = new Set(canvasData.edges
@@ -124,6 +181,30 @@ export default class CanvasFilterPlugin extends Plugin {
 					.map(x => x.id))
 
 				showOnlyEdges(canvas, shownEdgeIds);
+			})
+		});
+
+		this.addCommand({
+			id: 'show-connected-nodes-from-to',
+			name: 'Show nodes and groups: selected ARROWS TO/FROM',
+			checkCallback: this.ifActiveViewIsCanvas((canvas, canvasData) => {
+				this.showConnectedNodes(canvas, canvasData, true, true)
+			})
+		});
+
+		this.addCommand({
+			id: 'show-connected-nodes-from',
+			name: 'Show nodes and groups: selected ARROWS FROM',
+			checkCallback: this.ifActiveViewIsCanvas((canvas, canvasData) => {
+				this.showConnectedNodes(canvas, canvasData, true, false)
+			})
+		});
+
+		this.addCommand({
+			id: 'show-connected-nodes-to',
+			name: 'Show nodes and groups: selected ARROWS TO',
+			checkCallback: this.ifActiveViewIsCanvas((canvas, canvasData) => {
+				this.showConnectedNodes(canvas, canvasData, false, true)
 			})
 		});
 	}
